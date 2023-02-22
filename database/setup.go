6cc1a4704f17b478849093765e455795f9e6c9a6
Karta Kusuma <kartakusuma1199@gmail.com>
2023-02-22 22:50:20 +0700

    Initializing project

---
+++ database/setup.go
@@ -0,0 +1,21 @@
+package database
+
+import (
+	"goods-inventory-restapi/models"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Connection() {
+	db, err := gorm.Open(mysql.Open("root:@tcp(localhost:3306)/goods_inventory"))
+	if err != nil {
+		panic(err)
+	}
+
+	db.AutoMigrate(&models.Goods{})
+
+	DB = db
+}
