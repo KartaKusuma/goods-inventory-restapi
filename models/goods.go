9006d6181808539531a6ff65b48875f85fea7bca
Karta Kusuma <kartakusuma1199@gmail.com>
2023-02-22 23:16:16 +0700

    Modify Goods model

--- models/goods.go
+++ models/goods.go
@@ -1,4 +1,8 @@
 package models
 
 type Goods struct {
+	Id          uint   `json:"id" gorm:"primaryKey"`
+	Name        string `json:"name" gorm:"type:varchar(255)"`
+	Description string `json:"description" gorm:"type:text"`
+	Stock       int    `json:"stock" gorm:"type:integer"`
 }
